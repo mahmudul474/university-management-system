517bbf47ec76d57aedfaff685060f4f66957ecda
unknown <mahmudul474>
2023-06-03 02:10:56 +0600

    dot env done

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,19 @@
-console.log("branch check");
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log("listening on port", config.port);
+    });
+
+    console.log("database connection established successfully ");
+  } catch (e) {
+    console.log("feild to db connect ", e);
+  }
+}
+
+main().catch(err => console.log(err));
